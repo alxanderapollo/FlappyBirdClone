751665b4ef5eabb1ae55e92702527a763d5adc90
Alex Salazar <asalazar66@gatech.edu>
2025-04-24 17:33:49 -0400

    added character control so now the player can move up and down

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float flapForce = 5f;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = Vector2.up * flapForce;
+        }
     }
 }
