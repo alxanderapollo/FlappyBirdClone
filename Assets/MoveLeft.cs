5dc09fac39c691d6cfad78d2a3fcb07ac8604796
Alex Salazar <asalazar66@gatech.edu>
2025-04-25 19:39:12 -0400

    got the ground to respawn to the right - missing pipe respawn - tomorrow working on UI to create a health system, and then colliders so that we loose life when we collide with the world

--- Assets/MoveLeft.cs
+++ Assets/MoveLeft.cs
@@ -3,9 +3,22 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 2f;
+    private float groundWidth;
+
+    void Start()
+    {
+        // Get width of the ground sprite
+        groundWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
 
     void Update()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
+
+        // If the ground moves too far left, reset its position to the right
+        if (transform.position.x < -groundWidth)
+        {
+            transform.position += Vector3.right * groundWidth * 2f;
+        }
     }
 }
