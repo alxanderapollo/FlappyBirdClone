c624185a0e630db642643e213d2b9c25fbd9293a
Alex Salazar <asalazar66@gatech.edu>
2025-04-29 21:34:27 -0400

    added some sound effects - missing maybe a small explosive crash against the sprite when the plane downs - missing adding increase scaled diffculty

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,13 @@ public class PlayerController : MonoBehaviour
 {
     public float flyForce = 5f;
     [SerializeField] private float _rotationSpeed = 10f;
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip crashSound;
+    [SerializeField] private AudioClip jumpSound;
     private Rigidbody2D rb;
+
+    private bool hasCrashed = false;
 
     void Start()
     {
@@ -20,15 +26,21 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.linearVelocity = Vector2.up * flyForce;
+            audioSource.PlayOneShot(jumpSound, 0.15f);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-
-        Destroy(gameObject);
+        audioSource.PlayOneShot(crashSound);
+        // Destroy(gameObject);
         Time.timeScale = 0;
         GameOverManager gameOverManager = FindObjectOfType<GameOverManager>();
         gameOverManager.ShowGameOver();  // Trigger the GameOver screen
+        AudioSource engineHum = FindObjectOfType<AudioSource>();
+        engineHum.Stop();
+        GetComponent<SpriteRenderer>().enabled = false;   // Hide sprite
+        GetComponent<Collider2D>().enabled = false;       // Stop collisions
+        rb.simulated = false;
     }
 }
